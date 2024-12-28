78c58c5a2778da6835487b1149547a82b03a0d4f
Sebastian Faltoni <sebastian@nukedbit.io>
2024-12-28 19:10:25 +0100

    fix test settings

--- src/ObsidianGameStudios.Test.Framework.Tests/GlobalDatabaseSetupFixture.cs
+++ src/ObsidianGameStudios.Test.Framework.Tests/GlobalDatabaseSetupFixture.cs
@@ -1,10 +1,10 @@
 ﻿using ObsidianGameStudios.NUnit.Database;
 
-[assembly: LevelOfParallelism(1)]
+[assembly: LevelOfParallelism(2)]
 
 namespace ObsidianGameStudios.Test.Framework.Tests;
 
-[DatabaseFixtureGenerator("SQL_SERVER_DB_CONNECTION_STRING", DatabaseProvider.SqlServer, 10, dropDatabase: false)]
+[DatabaseFixtureGenerator("SQL_SERVER_DB_CONNECTION_STRING", DatabaseProvider.SqlServer, 2, dropDatabase: false)]
 public partial class GlobalDatabaseSetupFixture
 {
 
